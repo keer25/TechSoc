289ac2977f4947607ab22dfe53dac109fb91601d
MyRa-7 <MyRa-7@users.noreply.github.com>
2016-03-12 11:43:21 +0530

    Added Hostellers Page, I think

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
   root 'static_pages#home'
   get 'contact' => 'static_pages#contact'
+  get 'hostellers' => 'static_pages#hostellers'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
