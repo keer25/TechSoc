49fc89a87bc38559c13a70ed6c4506231a3b80ef
MyRa-7 <MyRa-7@users.noreply.github.com>
2016-03-12 12:57:46 +0530

    Addded Executive

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :sessions
   get 'performance' => 'static_pages#performance' 
   get 'gallery' => 'static_pages#gallery'
+  get 'executive' => 'static_pages#executive'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
