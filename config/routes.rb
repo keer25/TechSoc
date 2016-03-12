2e2c0914dd9b98b3027fa4e5220c30ffa4c0010d
KeerthanaS <keerukeerthana8@gmail.com>
2016-03-12 11:03:05 +0530

    Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root 'static_pages#home'
+  get 'contact' => 'static_pages#contact'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
